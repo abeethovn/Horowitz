d3eb1947b33549c5d2b9ebf2f60f8e352cad5619
A Beethovn <a.beethovn@gmail.com>
2012-08-21 15:19:21 -0700

    begin

--- Horowitz/src/Horowitz.java
+++ Horowitz/src/Horowitz.java
@@ -5,9 +5,24 @@ public class Horowitz {
 	public static void main(String[] args) {
 		
 		// this is a hot fix
+		//
+		//
+		//
+		//
+		//
+		
 		// i have fixed some stuff here
-		// c3
-		// c4
+		
+		
+		
+		/*
+		 * and this is where i change issue 53
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
 
 	}
 
