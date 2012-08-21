908694be2d390cd01823b80bf2a2767f4db7d6cb
A Beethovn <a.beethovn@gmail.com>
2012-08-21 15:40:36 -0700

    cleaned up

--- Horowitz/src/Horowitz.java
+++ Horowitz/src/Horowitz.java
@@ -5,24 +5,17 @@ public class Horowitz {
 	public static void main(String[] args) {
 		
 		// this is a hot fix
-		//
 		// I have fixed some code here
 		// this is my hot fix
 		//some stuff here
 		
-		
-		
 		/*
 		 * and this is where i change issue 53
-		 * 
 		 * i am such a great programmer
 		 * cuz i have fixed issue 53 here
 		 * and i am ready to merge it here
-		 * 
 		 * and let's add another line after the marge
-		 * 
 		 */
-
 	}
 
 }
